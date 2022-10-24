ea2d056dfdfdef2e5d538ca805c64d603ac9c1a8
canergolbasi <canergolbasi@gmail.com>
2022-10-24 20:46:41 +0300

    IfElse

---
+++ Introduction/IfElse.java
@@ -0,0 +1,38 @@
+/*
+In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes.
+Given an integer, , perform the following conditional actions:
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+ */
+
+
+import java.util.*;
+
+
+public class IfElse {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        
+        if((N%2)==1){
+            System.out.println("Weird");
+        }else if(N%2==0 && N>2 && N<=5){
+            System.out.println("Not Weird");
+        }else if(N%2==0 && N>=6 && N<=20){
+            System.out.println("Weird");
+        }else if(N%2==0 && N>20){
+            System.out.println("Not Weird");
+        }
+    }
+}
+
